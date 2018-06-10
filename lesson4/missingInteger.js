36dd62a0c3ef421aa1361e879f81a6e9752bf277
Alex Shneyderman <a.shneyderman@gmail.com>
2018-06-10 13:38:38 -0400

    We can optimize a bit more and combine de-duplication with search for the mininmum value

--- lesson4/missingInteger.js
+++ lesson4/missingInteger.js
@@ -1,35 +1,27 @@
 const _ = require('lodash')
 
 function solution(A) {
-
   const F = A.filter((v) => (v > 0));
-
   if(F.length === 0) {
     return 1;
   }
 
   const S = F.sort((l, r) => (l - r));
-  const D = S.reduce((acc, v) => {
-    if(acc.length == 0 || acc[acc.length - 1] !== v) {
-      acc.push(v); 
-    }
-    return acc;
-  }, []);
-
-  if(D[0] !== 1) {
+  if(S[0] !== 1) {
     return 1;
   }
 
   let result = -1;
-  for(let i = 1; i < D.length; i++) {
-    if(D[i] !== i+1) {
-      result=i+1;
+  for(let i = 1; i < S.length; i++) {
+    if(S[i] === S[i-1]) { continue; }
+    if(S[i] !== (S[i-1] + 1)) {
+      result=S[i-1] + 1;
       break;
     }
   }
 
   if(result == -1) {
-    return D.length + 1;
+    return S[S.length-1] + 1;
   }
 
   return result;
